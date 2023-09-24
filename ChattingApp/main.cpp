62774116ace2996623bccf0029cbfaf827fca267
Mertkan Kececi <47403024+mertkan-k@users.noreply.github.com>
2023-09-25 02:13:34 +0300

    Add project files.

---
+++ ChattingApp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "server start." << endl;
+
+	cout << "server end." << endl;
+	return 0;
+}
---
+++ Client/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "client start." << endl;
+
+	cout << "client end." << endl;
+	return 0;
+}
